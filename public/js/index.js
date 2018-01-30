633f2dd96f2eb7900bcd31ad79d86f242c24b326
Damir Pihljak <damir.pihljak@gmail.com>
2018-01-30 21:05:54 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'jen@example.com',
-        text: 'Hej. Šta ima?'
-    });
 });
 
 socket.on('disconnect', function () {
